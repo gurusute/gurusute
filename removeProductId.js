3b9d04ddddf60e5c6c79b869d33eb7f5b52f0227
shogito <next1@kabushikikaisha-no-iMac.local>
2014-12-03 10:36:00 +0900

    ADD: removeProductId.js

---
+++ removeProductId.js
@@ -0,0 +1,16 @@
+// HTMLの一番下辺りで読み込み
+// 商品名が表示される部分は全て<span class="productname"></span>で囲む
+
+function removeProductIds(){
+    // .textを置換する関数
+    function removeProductId(){
+       $(this).text($(this).text().replace(/\[商品番号:\d+\]/,""));
+    }
+
+    // 全てのproductnameクラスに適用
+    $(".productname").each(removeProductId)
+}
+
+// メイン処理
+removeProductIds()
+
